9a81a4528d84ac07566899305fa56e4b7bf96713
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-10-23 05:20:07 +0600

    done with useMemo and useCallback hooks

--- src/Pages/Counter.js
+++ src/Pages/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Button from "../components/Counter/Button";
 import ShowCount from "../components/Counter/ShowCount";
 import Title from "../components/Counter/Title";
@@ -6,16 +6,22 @@ import Title from "../components/Counter/Title";
 const Counter = () => {
     const [count1, setCount1] = useState(0);
     const [count2, setCount2] = useState(0);
-    const incrementByOne = () => {
+    const incrementByOne = useCallback(() => {
         setCount1((prevCount) => prevCount + 1);
-    };
-    const incrementByFive = () => {
+    }, []);
+    const incrementByFive = useCallback(() => {
         setCount2((prevCount) => prevCount + 5);
-    };
+    }, []);
+    const isEvenOrOdd = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) i++;
+        return count1 % 2 === 0 ? "Even" : "Odd";
+    }, [count1]);
     return (
         <div className="container mx-auto mt-5">
             <Title />
             <ShowCount count={count1} title="Counter 01" />
+            <p>Count 1 is {isEvenOrOdd}</p>
             <Button handleClick={incrementByOne}>Increament by 1</Button>
             <hr className="my-5" />
             <ShowCount count={count2} title="Counter 02" />
--- src/components/Counter/ShowCount.js
+++ src/components/Counter/ShowCount.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ShowCount = ({ count, title }) => {
+    console.log(`rendering showCount... ${title}`);
     return (
         <div>
             {title} is <span className="font-medium text-red-500">{count}</span>
